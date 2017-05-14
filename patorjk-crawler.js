9f69eea9431bcda0964e4d9f2cea636ff6df8b6f
Sauloxd <saulotoshi@gmail.com>
2017-05-13 23:29:44 -0300

    refactored patorjk

--- patorjk-crawler.js
+++ patorjk-crawler.js
@@ -12,39 +12,35 @@ var Horseman  = require('node-horseman')
 scavengePatorjk()
 
 function scavengePatorjk() {
-  getFontList()
+  var argv = require('minimist')(process.argv.slice(2))
+
+  getFontList(argv)
     .then(getAlphabetForEveryFont)
-    .then((asciiFonts) => {
-      fs.writeFileAsync('asciiFontsByGroup.json', JSON.stringify(asciiFonts, null,
-      2), 'utf8', 2)
-    })
 }
 
 function getAlphabetForEveryFont(fontList) {
-  console.log('Looking for...')
-  console.log(fontList[cte.target])
-  return Promise.mapSeries(fontList[cte.target], (fontFace) => {
-    console.log('current alphabet: ', cte.alphabet)
-    return Promise.mapSeries(cte.alphabet, phantomCrawlForFontByLetter.bind({fontFace:
-    fontFace}))
+  return Promise.mapSeries(fontList[cte.target], getForEveryAlphabet)
+    .then(asciiFonts => {
+      return _.reduce(asciiFonts, (groupedFontAscii, asciiFont, index) => {
+        console.log('index', index)
+        groupedFontAscii[fontList[cte.target][index]] = asciiFont
+        return groupedFontAscii
+      }, {})
+    })
+}
+
+function getForEveryAlphabet(fontFace) {
+  return Promise.mapSeries(cte.alphabet, phantomCrawlForFontByLetter.bind({fontFace:
+  fontFace}))
     .then(asciiAlphabet => {
       return _.reduce(asciiAlphabet, (groupedAsciiAlphabet, asciiLetter, index) => {
         groupedAsciiAlphabet[cte.alphabet[index]] = asciiLetter
         return groupedAsciiAlphabet
       }, {}) // => { A: '   __ - ; ', B: 'other bizarre string }
     })
-    .then((r) => {console.log('alfabeto inteiro:\n', r); return r})
-  })
-  .then(asciiFonts => {
-    // [{}, {}, ..]
-    return _.reduce(asciiFonts, (groupedFontAscii, asciiFont, index) => {
-      console.log('asciiFont', fontList)
-      console.log('index', index)
-      groupedFontAscii[fontList[cte.target][index]] = asciiFont
-      return groupedFontAscii
-    }, {})
-  })
-  .then((r) => {console.log('todas as fontes:\n', r); return r})
+    .then((asciiFont) => {
+      return fs.writeFileAsync('fonts/' + fontFace + '.json', JSON.stringify(asciiFont, null, 2), 'utf8', 2)
+    })
 }
 
 function phantomCrawlForFontByLetter(letter) {
@@ -57,34 +53,51 @@ function phantomCrawlForFontByLetter(letter) {
     .open(path)
     .text('#taag_output_text')
     .then((result) => { console.log(result); horseman.close(); return result })
+    .catch((err) => {
+      if (fs.existsSync('fonts/' + self.fontFace + '.json'))
+        return fs.unlinkAsync('fonts/' + self.fontFace + '.json')
+          .then(() => {
+            return fs.writeFileAsync('.checkpoint.json', JSON.stringify({err: err, fontFace: self.fontFace, letter: letter}, null, 2), 'utf8', 2)
+          })
+
+      return fs.writeFileAsync('.checkpoint.json', JSON.stringify({err: err, fontFace: self.fontFace, letter: letter}, null, 2), 'utf8', 2)
+    })
 }
 
-function getFontList(body) {
+function getFontList(argv) {
   var fontIndexPath = './fontIndex.json'
-  if (!fs.existsSync(fontIndexPath)) {
-    var path = '/software/taag/#p=display&f=Alpha&t=A' //random Path just to get FontList
-    var horseman = new Horseman()
-    return horseman
-      .open(site + path)
-      .html() //TODO: it's improvable
-      .then(body => {
-        var $ = cheerio.load(body)
-        var fontList = _.reduce($('#fontList').children(),
-          (fontGroupJson, fontGroup) => {
-            fontGroupJson[fontGroup.attribs.label] = _.filter(_.map(fontGroup.children, (fontType) => {
-              return $(fontType).text()
-            }), (fontType) => { return fontType !== '\n' && !_.isEmpty(fontType) }) //removes \n from array
-            return fontGroupJson
-          },
-          {})
+  if (fs.existsSync(fontIndexPath)) {
+    return fs.readFileAsync(fontIndexPath, 'utf8')
+      .then(file => {
+        const defaultGroup = 'Featured FIGlet Fonts'
+        var file = JSON.parse(file)
+        var index = (argv.f) ? file[argv.g || defaultGroup].indexOf(argv.f) : 0
+        file[argv.group || defaultGroup] = file[argv.group || defaultGroup].slice(index)
 
-        horseman.close()
-        //Saving font to a JSON file
-        return fs.writeFileAsync(fontIndexPath, JSON.stringify(fontList, null,
-        2), 'utf8', 2).then(() => { return fontList })
+        return file
       })
-  } else {
-    return fs.readFileAsync(fontIndexPath, 'utf8').then(file => { return JSON.parse(file) })
   }
+
+  var path = '/software/taag/#p=display&f=Alpha&t=A' //random Path just to get FontList
+  var horseman = new Horseman()
+  return horseman
+    .open(site + path)
+    .html() //TODO: it's improvable
+    .then(body => {
+      var $ = cheerio.load(body)
+      var fontList = _.reduce($('#fontList').children(),
+        (fontGroupJson, fontGroup) => {
+          fontGroupJson[fontGroup.attribs.label] = _.filter(_.map(fontGroup.children, (fontType) => {
+            return $(fontType).text()
+          }), (fontType) => { return fontType !== '\n' && !_.isEmpty(fontType) }) //removes \n from array
+          return fontGroupJson
+        },
+        {})
+
+      horseman.close()
+      //Saving font to a JSON file
+      return fs.writeFileAsync(fontIndexPath, JSON.stringify(fontList, null,
+      2), 'utf8', 2).then(() => { return fontList })
+    })
 }
 
